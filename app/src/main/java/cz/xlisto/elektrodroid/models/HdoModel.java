30e7712b3d4dc4d02c8b46e858a6275fcc27d90b
Xlisto <x.listo@seznam.cz>
2024-08-20 02:40:28 +0200

    Přidána dokumentace ke všem metodám třídy `HdoModel`.
    - Přidány Javadoc komentáře k metodám `setId`, `getId`, `getRele`, `getDateFrom`, `getDateUntil`, `getTimeFrom`, `getTimeUntil`, `getMon`, `getTue`, `getWed`, `getThu`, `getFri`, `getSat`, `getSun`, `getDistributionArea`, `setCalendar`, `setCalendarStart`, `setCalendarEnd`, `getCalendarStart`, `getCalendarEnd`, `clone`, `toString`, `getDates`, `equals`, `hashCode`, `setDayOfWeekPRE`.
    - Komentáře obsahují popis metod, parametrů a návratových hodnot.

--- app/src/main/java/cz/xlisto/elektrodroid/models/HdoModel.java
+++ app/src/main/java/cz/xlisto/elektrodroid/models/HdoModel.java
@@ -1,6 +1,8 @@
 package cz.xlisto.elektrodroid.models;
 
 
+import android.util.Log;
+
 import androidx.annotation.NonNull;
 
 import java.util.Calendar;
@@ -9,11 +11,15 @@ import java.util.Objects;
 
 
 /**
- * Model pro časy HDO
+ * Třída HdoModel představuje model pro časy HDO (Hromadné dálkové ovládání).
+ * <p>
+ * Tato třída implementuje rozhraní Cloneable, což umožňuje vytváření kopií objektů této třídy.
+ * <p>
  * Xlisto 26.05.2023 18:44
  */
 
 public class HdoModel implements Cloneable {
+
     private static final String TAG = "HdoModel";
     private long id;
     private final String rele;
@@ -25,10 +31,27 @@ public class HdoModel implements Cloneable {
     private Calendar calendarStart;
     private Calendar calendarEnd;
 
-
     private int mon, tue, wed, thu, fri, sat, sun;
 
 
+    /**
+     * Konstruktor třídy HdoModel.
+     *
+     * @param id               Identifikátor HDO modelu.
+     * @param rele             Rele HDO.
+     * @param dateFrom         Datum začátku platnosti HDO.
+     * @param dateUntil        Datum konce platnosti HDO.
+     * @param timeFrom         Čas začátku platnosti HDO.
+     * @param timeUntil        Čas konce platnosti HDO.
+     * @param mon              Indikátor platnosti HDO v pondělí (1 = platí, 0 = neplatí).
+     * @param tue              Indikátor platnosti HDO v úterý (1 = platí, 0 = neplatí).
+     * @param wed              Indikátor platnosti HDO ve středu (1 = platí, 0 = neplatí).
+     * @param thu              Indikátor platnosti HDO ve čtvrtek (1 = platí, 0 = neplatí).
+     * @param fri              Indikátor platnosti HDO v pátek (1 = platí, 0 = neplatí).
+     * @param sat              Indikátor platnosti HDO v sobotu (1 = platí, 0 = neplatí).
+     * @param sun              Indikátor platnosti HDO v neděli (1 = platí, 0 = neplatí).
+     * @param distributionArea Distribuční oblast HDO.
+     */
     public HdoModel(long id, String rele, String dateFrom, String dateUntil, String timeFrom, String timeUntil, int mon, int tue, int wed, int thu, int fri, int sat, int sun, String distributionArea) {
         this.id = id;
         this.rele = rele;
@@ -49,41 +72,93 @@ public class HdoModel implements Cloneable {
     }
 
 
+    /**
+     * Konstruktor třídy HdoModel.
+     *
+     * @param rele             Rele HDO.
+     * @param dateFrom         Datum začátku platnosti HDO.
+     * @param dateUntil        Datum konce platnosti HDO.
+     * @param timeFrom         Čas začátku platnosti HDO.
+     * @param timeUntil        Čas konce platnosti HDO.
+     * @param mon              Indikátor platnosti HDO v pondělí (1 = platí, 0 = neplatí).
+     * @param tue              Indikátor platnosti HDO v úterý (1 = platí, 0 = neplatí).
+     * @param wed              Indikátor platnosti HDO ve středu (1 = platí, 0 = neplatí).
+     * @param thu              Indikátor platnosti HDO ve čtvrtek (1 = platí, 0 = neplatí).
+     * @param fri              Indikátor platnosti HDO v pátek (1 = platí, 0 = neplatí).
+     * @param sat              Indikátor platnosti HDO v sobotu (1 = platí, 0 = neplatí).
+     * @param sun              Indikátor platnosti HDO v neděli (1 = platí, 0 = neplatí).
+     * @param distributionArea Distribuční oblast HDO.
+     */
     public HdoModel(String rele, String dateFrom, String dateUntil, String timeFrom, String timeUntil, int mon, int tue, int wed, int thu, int fri, int sat, int sun, String distributionArea) {
         this(-1, rele, dateFrom, dateUntil, timeFrom, timeUntil, mon, tue, wed, thu, fri, sat, sun, distributionArea);
     }
 
 
+    /**
+     * Nastaví identifikátor HDO modelu.
+     *
+     * @param id Identifikátor HDO modelu.
+     */
     public void setId(long id) {
         this.id = id;
     }
 
 
+    /**
+     * Vrátí identifikátor HDO modelu.
+     *
+     * @return Identifikátor HDO modelu.
+     */
     public long getId() {
         return id;
     }
 
 
+    /**
+     * Vrátí rele HDO.
+     *
+     * @return Rele HDO.
+     */
     public String getRele() {
         return rele;
     }
 
 
+    /**
+     * Vrátí datum začátku platnosti HDO.
+     *
+     * @return Datum začátku platnosti HDO.
+     */
     public String getDateFrom() {
         return dateFrom;
     }
 
 
+    /**
+     * Vrátí datum konce platnosti HDO.
+     *
+     * @return Datum konce platnosti HDO.
+     */
     public String getDateUntil() {
         return dateUntil;
     }
 
 
+    /**
+     * Vrátí čas začátku platnosti HDO.
+     *
+     * @return Čas začátku platnosti HDO.
+     */
     public String getTimeFrom() {
         return timeFrom;
     }
 
 
+    /**
+     * Vrátí čas konce platnosti HDO.
+     *
+     * @return Čas konce platnosti HDO.
+     */
     public String getTimeUntil() {
         if (timeUntil.equals("23:59") || timeUntil.equals("24:00"))
             return "0:00";
@@ -91,41 +166,81 @@ public class HdoModel implements Cloneable {
     }
 
 
+    /**
+     * Vrátí indikátor platnosti HDO v pondělí.
+     *
+     * @return Indikátor platnosti HDO v pondělí.
+     */
     public int getMon() {
         return mon;
     }
 
 
+    /**
+     * Vrátí indikátor platnosti HDO v úterý.
+     *
+     * @return Indikátor platnosti HDO v úterý.
+     */
     public int getTue() {
         return tue;
     }
 
 
+    /**
+     * Vrátí indikátor platnosti HDO ve středu.
+     *
+     * @return Indikátor platnosti HDO ve středu.
+     */
     public int getWed() {
         return wed;
     }
 
 
+    /**
+     * Vrátí indikátor platnosti HDO ve čtvrtek.
+     *
+     * @return Indikátor platnosti HDO ve čtvrtek.
+     */
     public int getThu() {
         return thu;
     }
 
 
+    /**
+     * Vrátí indikátor platnosti HDO v pátek.
+     *
+     * @return Indikátor platnosti HDO v pátek.
+     */
     public int getFri() {
         return fri;
     }
 
 
+    /**
+     * Vrátí indikátor platnosti HDO v sobotu.
+     *
+     * @return Indikátor platnosti HDO v sobotu.
+     */
     public int getSat() {
         return sat;
     }
 
 
+    /**
+     * Vrátí indikátor platnosti HDO v neděli.
+     *
+     * @return Indikátor platnosti HDO v neděli.
+     */
     public int getSun() {
         return sun;
     }
 
 
+    /**
+     * Vrátí distribuční oblast HDO.
+     *
+     * @return Distribuční oblast HDO.
+     */
     public String getDistributionArea() {
         if (distributionArea == null)
             return "";
@@ -207,18 +322,28 @@ public class HdoModel implements Cloneable {
     }
 
 
+    /**
+     * Vytvoří a vrátí kopii objektu HdoModel.
+     *
+     * @return Kopie objektu HdoModel.
+     */
     @NonNull
     @Override
     public HdoModel clone() {
         try {
             return (HdoModel) super.clone();
         } catch (CloneNotSupportedException e) {
-            e.printStackTrace();
+            Log.e(TAG, "clone: ", e);
             throw new AssertionError();
         }
     }
 
 
+    /**
+     * Vrátí textovou reprezentaci objektu HdoModel.
+     *
+     * @return Textová reprezentace objektu HdoModel.
+     */
     @NonNull
     @Override
     public String toString() {
@@ -264,6 +389,12 @@ public class HdoModel implements Cloneable {
     }
 
 
+    /**
+     * Porovná aktuální objekt HdoModel s jiným objektem.
+     *
+     * @param o Objekt, se kterým se má porovnat.
+     * @return true, pokud jsou objekty stejné, jinak false.
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -275,12 +406,20 @@ public class HdoModel implements Cloneable {
     }
 
 
+    /**
+     * Vrátí hash kód objektu HdoModel.
+     *
+     * @return Hash kód objektu HdoModel.
+     */
     @Override
     public int hashCode() {
         return Objects.hash(calendarStart.getTimeInMillis(), calendarEnd.getTimeInMillis(), rele);
     }
 
 
+    /**
+     * Nastaví dny v týdnu pro distribuční oblast PRE.
+     */
     private void setDayOfWeekPRE() {
         if (distributionArea.equals("PRE")) {
             int dayOfMonth = Integer.parseInt(dateFrom.split("\\.")[0]);
@@ -315,4 +454,5 @@ public class HdoModel implements Cloneable {
             }
         }
     }
+
 }
